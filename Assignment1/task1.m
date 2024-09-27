d380894ee919a8320ff3751d8702ffca68479f8b
felici24 <felici24@rowan.edu>
2024-09-27 15:52:40 +0000

    The deed is done father

---
+++ Assignment1/task1.m
@@ -0,0 +1,11 @@
+%Author Name: Vincent T. Feliciano
+% Email: felici24@rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Homework 1
+
+
+% a) Create a variable 'radius' and assign it a value of 5.
+% b) Calculae the area of a circle with this radius and store it in a variable called 'area'.
+% c) Calculate the circumference of the circle and store it in a variable called 'circumference'.
+% d) Display the results with appropriate labels
+
